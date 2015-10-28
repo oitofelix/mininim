71088752a4bf964e97c4168de1f731f182f050db
Bruno Félix Rezende Ribeiro <oitofelix@gnu.org>
2015-10-28 11:25:31 -0200

    Add wall collision detection (physics engine)

---
+++ engine/physics.c
@@ -0,0 +1,33 @@
+/*
+  physics.c -- physics module;
+
+  Copyright (C) 2015, 2016 Bruno Félix Rezende Ribeiro <oitofelix@gnu.org>
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 3, or (at your option)
+  any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "physics.h"
+#include "level.h"
+
+bool
+is_colliding (struct anim anim)
+{
+  if (! anim.collision) return false;
+
+  struct level_pos pos = anim_pos (anim);
+  enum level_object obj = level_obj (pos);
+  if (obj == WALL) {
+    return true;
+  } else return false;
+}
---
+++ engine/physics.h
@@ -0,0 +1,27 @@
+/*
+  physics.h -- physics module;
+
+  Copyright (C) 2015, 2016 Bruno Félix Rezende Ribeiro <oitofelix@gnu.org>
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 3, or (at your option)
+  any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef FREEPOP_PHYSICS_H
+#define FREEPOP_PHYSICS_H
+
+#include "anim.h"
+
+bool is_colliding (struct anim anim);
+
+#endif	/* FREEPOP_PHYSICS_H */
