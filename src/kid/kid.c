ca30cdfbd73900d90bec1dc7cf15d86aadf3d328
Bruno Félix Rezende Ribeiro <oitofelix@gnu.org>
2016-05-22 14:53:04 -0300

    - Bug fix: increase life (SHIFT+S and SHIFT+T) and float (SHIFT+W) commands take effect even when the kid is dead.

--- src/kid/kid.c
+++ src/kid/kid.c
@@ -435,22 +435,23 @@ kid_life_coord (int i, struct coord *c)
 void
 increase_kid_current_lives (struct anim *k)
 {
-  if (k->current_lives < k->total_lives) {
-    k->current_lives++;
-    if (! is_playing_sample (small_life_potion_sample))
-      play_sample (small_life_potion_sample, -1);
-    if (k->id == current_kid_id) {
-      mr.flicker = 8;
-      mr.color = get_flicker_blood_color ();
-    }
+  if (k->current_lives <= 0
+      || k->current_lives >= k->total_lives) return;
+
+  k->current_lives++;
+  if (! is_playing_sample (small_life_potion_sample))
+    play_sample (small_life_potion_sample, -1);
+  if (k->id == current_kid_id) {
+    mr.flicker = 8;
+    mr.color = get_flicker_blood_color ();
   }
 }
 
 void
 increase_kid_total_lives (struct anim *k)
 {
-  if (k->total_lives >= 10
-      && k->current_lives >= k->total_lives)
+  if ((k->total_lives >= 10 && k->current_lives >= k->total_lives)
+      || k->current_lives <= 0)
     return;
 
   if (k->total_lives < 10) k->total_lives++;
@@ -466,6 +467,7 @@ increase_kid_total_lives (struct anim *k)
 void
 float_kid (struct anim *k)
 {
+  if (k->current_lives <= 0 && ! is_kid_fall (&k->f)) return;
   al_set_timer_count (k->floating, 0);
   al_start_timer (k->floating);
   stop_sample (k->sample, scream_sample);
