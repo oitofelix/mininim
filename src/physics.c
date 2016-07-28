1a7061316862fe2053b78d1abd63df15289ff190
Bruno Félix Rezende Ribeiro <oitofelix@gnu.org>
2016-07-28 01:09:22 -0300

    Fix bug

--- src/physics.c
+++ src/physics.c
@@ -955,7 +955,7 @@ can_hang (struct frame *f, bool reverse, struct pos *hang_pos)
 
   /* if (! reverse) printf ("dist_coord = %f\n", d); */
 
-  if (is_kid_fall (&_f) && d > 17) return false;
+  if (is_kid_fall (&_f) && d > 18) return false;
 
   return true;
 }
