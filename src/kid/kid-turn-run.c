0c678d4a9556d7e80964ba9ec7b074c51f47e4fd
Bruno Félix Rezende Ribeiro <oitofelix@gnu.org>
2016-08-06 23:43:34 -0300

    Fix regression in collision detection

--- src/kid/kid-turn-run.c
+++ src/kid/kid-turn-run.c
@@ -115,12 +115,7 @@ physics_in (struct anim *k)
   k->inertia = k->cinertia = 0;
 
   /* collision */
-  if (is_colliding (&k->f, &k->fo, +0, 2, &k->ci)) {
-    uncollide (&k->f, &k->fo, &k->fo, +0, 2, &k->ci);
-    /* k->f.dir = (k->f.dir == LEFT) ? RIGHT : LEFT; */
-    /* kid_stabilize_collision (k); */
-    /* return false; */
-  }
+  uncollide (&k->f, &k->fo, &k->fo, +0, 2, &k->ci);
 
   /* fall */
   survey (_tb, pos, &k->f, NULL, &ptb, NULL);
